671401af39200b77514e75f211b24b6e8281ed12
Vishal Kuo <vishalkuo@hotmail.com>
2016-01-15 10:10:00 -0500

    Test Spark scala file

--- src/main/scala-2.11/TestSpark.scala
+++ src/main/scala-2.11/TestSpark.scala
@@ -1,8 +1,44 @@
+import org.apache.spark.{SparkContext, SparkConf}
+
 /**
   * Created by vishalkuo on 2016-01-14.
   */
 object TestSpark {
   def main(args: Array[String]): Unit = {
-    println(System.getProperty("user.dir"))
+    //
+    //    val data = sc.textFile(url.getPath(), 10).cache()
+    //    val url = getClass.getResource("/datasets/profitMetrics.csv")
+
+    val conf = new SparkConf()
+      .setMaster("local[2]").setAppName("Test App").set("spark.executor.memory", "1g")
+    val sc = new SparkContext(conf)
+    sc.setLogLevel("WARN")
+
+    val data = sc.textFile("src/main/resources/datasets/SparkLearnSet.csv")
+      .map(line => line.split(",")).map(purchaseRecord => (purchaseRecord(0), purchaseRecord(1), purchaseRecord(2)))
+
+    val numPurchases = data.count()
+
+    val uniqueUsers = data.map({
+      case(user, product, price) => user
+    }).distinct().count()
+
+    val totalRevenue = data.map({
+      case(user, product, price) => price.toDouble
+    }).fold(0)((acc, i) => acc + i)
+
+    val productByPopularity = data.map({
+      case (user, product, price) => (product, 1)
+    }).reduceByKey(_ + _)
+    .collect()
+    .sortWith((a, b) => a._2 > b._2 )
+
+    val mostPopular = productByPopularity(0)
+
+
+    println("num of purchases: " + numPurchases)
+    println("unique users: " + uniqueUsers)
+    println("total revenue: " + totalRevenue)
+    println("Most popular: " + mostPopular)
   }
 }
