c803eea345b833cc0ece72b0d73dda11e5df30d8
Vishal Kuo <vishalkuo@hotmail.com>
2016-01-15 15:19:22 -0500

    Process movies

--- src/main/scala-2.11/MovieLensDataset.scala
+++ src/main/scala-2.11/MovieLensDataset.scala
@@ -15,6 +15,30 @@ object MovieLensDataset {
     val sc = new SparkContext(conf)
     sc.setLogLevel("WARN")
 
+//    processUsers(sc)
+
+    processMovieDataset(sc)
+  }
+
+  def processMovieDataset(sc: SparkContext): Unit ={
+    val movieData = sc.textFile("src/main/resources/datasets/ml-100k/u.item")
+    val movieFields = movieData.map(lines => lines.split("\\|"))
+    val years = movieFields.map(fields => fields(2)).map({
+      x => try{
+        x.substring(x.length - 4).toInt
+      } catch {
+        case _: NumberFormatException | _: StringIndexOutOfBoundsException => 1900
+      }
+    })
+    val yearsFiltered = years.filter(x => x != 1900)
+
+    val ageAggregated = yearsFiltered.map(x => 1998 - x).countByValue().toArray.sortBy(_._1)
+    for (age <- ageAggregated){
+      println(age)
+    }
+  }
+
+  def processUsers(sc: SparkContext): Unit ={
     val userData = sc.textFile("src/main/resources/datasets/ml-100k/u.user")
     val userFields = userData.map(line => line.split("\\|"))
     val numOfUsers = userFields.count()
@@ -36,15 +60,15 @@ object MovieLensDataset {
       fieldArr => (fieldArr(3), 1)
     }).reduceByKey(_ + _).sortBy(_._2).collect()
 
-//    for (occupation <- occupationCount){
-//      println(occupation)
-//    }
+    //    for (occupation <- occupationCount){
+    //      println(occupation)
+    //    }
 
     //This and previous occupation count will be the exact same, except for sort order
     val occupationCountByValue = userFields.map(fieldArr => fieldArr(3)).countByValue()
 
-//    for (occupation <- occupationCountByValue){
-//      println(occupation)
-//    }
+    //    for (occupation <- occupationCountByValue){
+    //      println(occupation)
+    //    }
   }
 }
